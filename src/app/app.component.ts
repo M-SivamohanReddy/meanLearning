1080f534cf58b598517261591cc03a98b8d8612c
SIVAMOHAN REDDY <sivamohan.reddy@walkingtree.tech>
2018-11-26 10:40:28 +0530

    initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor(private apiService: ApiService) {
+
+  }
+
+  ngOnInit() {
+    this.apiService.getMessages();
+  }
 }
